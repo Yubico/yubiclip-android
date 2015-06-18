0c53758ffbd4c3bbef674881043d00be1096bba0
Kemiren <thekemiren@googlemail.com>
2015-06-18 22:40:26 +0200

    bug fix..

--- src/main/com/yubico/yubiclip/scancode/DEKeyboardLayout.java
+++ src/main/com/yubico/yubiclip/scancode/DEKeyboardLayout.java
@@ -55,7 +55,7 @@ public class DEKeyboardLayout extends KeyboardLayout {
             "ü",
             "+",
             "",
-            "<",
+            "#",
             "ö",
             "'",
             "^",
